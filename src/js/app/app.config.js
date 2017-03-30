a47bf47bf7f0e3182846d887d4dd85be9191b024
Jaivardhan <jvss1996@gmail.com>
2017-03-30 20:26:02 +0530

    First Commit

---
+++ src/js/app/app.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('try').
+	config(
+		function(
+			$routeProvider,
+			$locationProvider
+			){
+			$locationProvider.html5Mode({
+				enabled:true
+			})
+			
+			$routeProvider.
+				when("/", {
+					template: "<blog-list></blog-list>"
+				}).
+				when("/about", {
+					templateUrl: "/templates/about.html"
+				}).
+				when("/blog", {
+					template:"<blog-list></blog-list>",
+					//redirectTo: '/'
+				}).
+				when("/blog/:id", {
+					template:"<blog-detail></blog-detail>"
+				}).
+				otherwise({
+					template: "Not Found"
+				})
+		});
